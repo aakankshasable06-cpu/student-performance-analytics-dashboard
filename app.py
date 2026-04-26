394817a4e8bdc4a42810a7524d69ed0ceeda0d5b
Aakanksha Sable <aakankshasable06@gmail.com>
2026-04-26 08:25:41 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(
     page_icon="📊",
     layout="wide"
 )
-st.title("📊 Student Performance Analytics Dashboard")
+st.title("📊 Dynamic Student Performance Analytics Dashboard")
 st.markdown("""
 <style>
 .stApp {
@@ -59,7 +59,8 @@ if file is not None:
    
     with tab1:
         st.subheader("📂 Uploaded Student Dataset")
-        row_df =filtered_df[['Name' ,'Maths','Science','English','Computer',"Attendance"]]
+        subjects= a.get_subjects(filtered_df)
+        row_df =filtered_df[['Name'] + subjects + ["Attendance"]]
         st.dataframe(row_df)
     with tab2:
         st.subheader("📈 Student Performance Analysis")
@@ -74,51 +75,52 @@ if file is not None:
         st.subheader("📌 Class Performance Summary")
         if filtered_df.empty:
              st.warning("No students match selected filters.")
-             st.stop()
-        col1, col2, col3, col4, col5 = st.columns(5)
-        with col1:
-            st.metric("👨‍🎓 Total Students", a.total_student(filtered_df))
+        else:    
+            col1, col2, col3, col4, col5 = st.columns(5)
+            with col1:
+                st.metric("👨‍🎓 Total Students", a.total_student(filtered_df))
 
-        with col2:
-            st.metric("Passed",a.pass_count(filtered_df))
+            with col2:
+                st.metric("Passed",a.pass_count(filtered_df))
 
-        with col3:
-            st.metric("Failed",a.fail_count(filtered_df))
+            with col3:
+                st.metric("Failed",a.fail_count(filtered_df))
 
-        with col4:
-            st.metric("High Risk",a.high_risk_count(filtered_df))
+            with col4:
+                st.metric("High Risk",a.high_risk_count(filtered_df))
 
-        with col5:
-            st.metric("Class Average",round(a.class_average(filtered_df),2)) 
-        st.divider()
-        topper = a.top_student(filtered_df)  
-        st.subheader("🏆 Top Performer")  
-        st.write(
-            f'{topper["Name"]} - {round(topper["Percentage"],2)}%'
+            with col5:
+                st.metric("Class Average",round(a.class_average(filtered_df),2)) 
+            st.divider()
+            topper = a.top_student(filtered_df)  
+            st.subheader("🏆 Top Performer")  
+            st.write(
+                f'{topper["Name"]} - {round(topper["Percentage"],2)}%'
+                )
+            csv = filtered_df.to_csv(index=False).encode("utf-8")
+            st.download_button(
+              label="⬇ Download Full Report",
+              data=csv,
+              file_name="student_analysis_report.csv",
+              mime="text/csv"
             )
-        csv = filtered_df.to_csv(index=False).encode("utf-8")
-        st.download_button(
-        label="⬇ Download Full Report",
-        data=csv,
-        file_name="student_analysis_report.csv",
-        mime="text/csv"
-        )
-        st.divider()
-        chart1,chart2 = st.columns(2)
-        with chart1:
-            st.subheader("📈 Grade Distribution")
-            grade_counts = a.grade_count(filtered_df)
-            st.bar_chart(grade_counts)
+            st.divider()
+            chart1,chart2 = st.columns(2)
+            with chart1:
+                st.subheader("📈 Grade Distribution")
+                grade_counts = a.grade_count(filtered_df)
+                st.bar_chart(grade_counts)
 
 
-        with chart2:
+            with chart2:
 
-            st.subheader("⚠ Risk Distribution")
-            risk_counts=a.risk_count(filtered_df)
-            st.bar_chart(risk_counts)
+                st.subheader("⚠ Risk Distribution")
+                risk_counts=a.risk_count(filtered_df)
+                st.bar_chart(risk_counts)
         
 else:
    st.info("Upload a CSV file to start analysis 📁")     
+    
 
 
 
